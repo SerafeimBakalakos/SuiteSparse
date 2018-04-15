2d6a4b4eb6f3c5b52d6b28d08a73f21e461acb1b
Serafeim Bakalakos <serafeim.bakalakos@outlook.com>
2018-04-15 19:28:32 +0300

    Added interface for row addition/deletion in a factorized matrix
    Signed-off-by: Serafeim Bakalakos <serafeim.bakalakos@outlook.com>

--- build/suitesparse_utilities/suitesparse_utilities.h
+++ build/suitesparse_utilities/suitesparse_utilities.h
@@ -4,28 +4,28 @@
 extern "C"
 {
 	/*
-	Allocates in heap and returns a handle that must be passed to all CHOLMOD functions.
-	*/
+	 * Allocates in heap and returns a handle that must be passed to all CHOLMOD functions.
+	 */
 	__declspec(dllexport) cholmod_common* util_create_common();
 
 	/*
-	Frees the memory for the handle.
-	*/
+	 * Frees the memory for the handle.
+	 */
 	__declspec(dllexport) void util_destroy_common(cholmod_common** common);
 
 	/*
-	Frees the memory for the factorized matrix.
-	*/
+	 * Frees the memory for the factorized matrix.
+	 */
 	__declspec(dllexport) void util_destroy_factor(cholmod_factor** factorized_matrix, cholmod_common* common);
 
 	/*
-	Factorize a symmetric matrix using cholesky algorithm. The matrix is in csc form, with only the upper triangle stored.
-	If cholesky is successful -1 is returned and out_factorized_matrix points to the factorized upper triangle.
-	If the matrix is not positive definite then the index (0-based) of the column where cholesky failed  
-	and out_factorized_matrix = NULL.
-	If the something another failure occurs, such as memory not being sufficient due to excessive fill-in then -2 is returned  
-	and out_factorized_matrix = NULL.
-	*/
+	 * Factorize a symmetric matrix using cholesky algorithm. The matrix is in csc form, with only the upper triangle stored.
+	 * If cholesky is successful -1 is returned and out_factorized_matrix points to the factorized upper triangle.
+	 * If the matrix is not positive definite then the index (0-based) of the column where cholesky failed  
+	 * and out_factorized_matrix = NULL.
+	 * If the something another failure occurs, such as memory not being sufficient due to excessive fill-in then -2 is returned  
+	 * and out_factorized_matrix = NULL.
+	 */
 	__declspec(dllexport) int util_factorize_cscupper(
 		int order, /*Number of rows = number of columns*/
 		int nnz, /*Number of non zero entries in the upper triangle.*/
@@ -36,11 +36,35 @@ extern "C"
 		cholmod_common* common);
 
 	/*
-	Solves a linear system with a single right hand side vector.
-	*/
+	 * Adds a row and column to an LDL' factorization. Before updating the kth row and column of L must be equal to the kth  
+	 * row and column of the identity matrix. The row/column to add must be a sparse CSC matrix with dimensions n-by-1,  
+	 * where n is the order of the matrix.	Returns 1 if the method succeeds, 0 otherwise.
+	 */
+	_declspec(dllexport) int util_row_add(
+		int order, /*Number of rows = number of columns*/
+		cholmod_factor* L, /*The LDL' factorization of the matrix. It will be modified.*/
+		int k, /*Index of row/column to add*/
+		int vector_nnz, /*Number of non zero entries of the row/column to add.*/
+		double* vector_values, /*The CSC format values of the row/column to add.*/
+		int* vector_row_indices, /*The CSC format row indices of the row/column to add.*/
+		int* vector_col_offsets, /*The CSC format column offsets of the row/column to add.*/
+		cholmod_common* common);
+
+	/*
+	 * Deletes a row and column from an LDL' factorization. After updating the kth row and column of L will be equal to the kth 
+	 * row and column of the identity matrix. Returns 1 if the method succeeds, 0 otherwise.
+	 */
+	_declspec(dllexport) int util_row_delete(
+		cholmod_factor* L, /*The LDL' factorization of the matrix. It will be modified.*/
+		int k, /*Index of row/column to add*/
+		cholmod_common* common);
+
+	/*
+	 * Solves a linear system with a single right hand side vector.
+	 */
 	__declspec(dllexport) void util_solve(
 		int order /*Number of matrix rows = number of matrix columns = length of right hand side vector*/,
-		cholmod_factor* factorized_matrix, /*The matrix after being factorized.*/
+		cholmod_factor* factorized_matrix, /*The LDL' factorization of the matrix.*/
 		double* rhs, /*The right hand side vector. Its length must be equal to the order of the matrix: factorized_matrix->n*/
 		double* out_solution, /*Buffer for the left hand side vector (unknown). Its length must be equal to the order of the matrix: factorized_matrix->n*/
 		cholmod_common* common);
